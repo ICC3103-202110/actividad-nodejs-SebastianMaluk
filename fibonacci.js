5a8cc34331d36923a11c351447edfff239134fc6
SebastianMaluk <samaluk@miuandes.cl>
2021-05-01 23:43:15 -0400

    Update fibonacci.js
    created function fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,1 +1,17 @@
-console.log('Hello world!');
+const readline = require("readline-sync");
+
+let number = readline.question("What number?\n");
+
+function fibonacci(number) {
+    if (number < 0){
+        return "Incorrect input, it must be positive"
+    } else if (number == 0){
+        return 0
+    } else if (number == 1){
+        return 1
+    } else {
+        return fibonacci(number-1) + fibonacci(number-2)
+    }
+}
+
+console.log(fibonacci(number))
